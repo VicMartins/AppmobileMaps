e385ef5281771caea17ef8d5fd8eb70d5b01e2cf
VicMartins <vic.martins0907@gmail.com>
2019-08-30 03:31:57 +0900

    mapa aparente

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,14 @@
 // This is a JavaScript file
 
+$(document).on('click',"#mapa",function(){
+  $('#mapa').hide();
+
+L.mapquest.key = 'yeBwU2m4gDsqcDjhiTViz2OOa12wVqT9';
+
+// 'map' refers to a <div> element with the ID map
+L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+});
