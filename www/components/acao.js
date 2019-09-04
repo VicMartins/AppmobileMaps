87456f572737f8266f3af0368258d9af234d6166
VicMartins <vic.martins0907@gmail.com>
2019-09-05 06:49:12 +0900

    finalizada por hora o JS

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,14 +1,49 @@
 // This is a JavaScript file
 
 $(document).on('click',"#mapa",function(){
-  $('#mapa').hide();
 
-L.mapquest.key = 'yeBwU2m4gDsqcDjhiTViz2OOa12wVqT9';
+  function checkConnection() {
+    var networkState = navigator.connection.type;
 
-// 'map' refers to a <div> element with the ID map
-L.mapquest.map('map', {
-  center: [37.7749, -122.4194],
-  layers: L.mapquest.tileLayer('map'),
-  zoom: 12
-});
+    var states = {};
+    states[Connection.UNKNOWN]  = '';
+    states[Connection.ETHERNET] = '';
+    states[Connection.WIFI]     = '';
+    states[Connection.CELL_2G]  = '';
+    states[Connection.CELL_3G]  = '';
+    states[Connection.CELL_4G]  = '';
+    states[Connection.CELL]     = '';
+    states[Connection.NONE]     = '';
+
+    if(networkState!= Connection.NONE){
+       navigator.notification.beep(1);
+        $('#mapa').hide();
+         var onSuccess = function(position) {
+           L.mapquest.key = 'yeBwU2m4gDsqcDjhiTViz2OOa12wVqT9';
+
+      // 'map' refers to a <div> element with the ID map
+      L.mapquest.map('map', {
+        center: [position.coords.latitude , position.coords.longitude],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 17
+      });
+       };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+    else{
+      alert("Você não está conectado a internet");
+      navigator.notification.beep(3);
+      navigator.vibrate(6000);
+    }
+}
+
+checkConnection();
+
 });
